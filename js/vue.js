e0fb0f6ffb4b17e5711fe484ffbb5233f51b9eb6
ayman <aymanabdullahjamal@gmail.com>
2019-11-09 13:41:02 +0800

    added array of objects about different centres
    added:
    name, image, desc,website

--- js/vue.js
+++ js/vue.js
@@ -1,7 +1,50 @@
 var app = new Vue({
     el: '#vue-app',
     data: {
-        centres: ['I read Quran Centre','Andalus','Aspire Institute','Darul Huffaz','Darul Quran','Yameen','Muhd Zuhaili'],
-        abouts: ['We provide a secure and conducive environment for the holistic development of each child physically, socially, morally, emotionally and intellectually.','Menyediakan pendidikan Islam dengan kesungguhan, kesabaran dan keikhlasan dalam melahirkan Muslim yang berilmu dan bertaqwa berteraskan al-Quran dan al-Sunnah.','For students who can already read Al-Quran and want to start to understand Al-Quran from basics.']
+        centres: [
+            {
+                name: "I read Quran Centre",
+                image: "i-read-quran-centre.jpg",
+                description: "We provide a secure and conducive environment for the holistic development of each child physically, socially, morally, emotionally and intellectually.",
+                website: "http://i-readquran.org/"
+            },
+            {
+                name: "Andalus",
+                image: "andalus.jpg",
+                description: "Menyediakan pendidikan Islam dengan kesungguhan, kesabaran dan keikhlasan dalam melahirkan Muslim yang berilmu dan bertaqwa berteraskan al-Quran dan al-Sunnah.",
+                website:"http://www.andalus.sg/webandalus/index.php"
+            },
+            {
+                name: "Aspire Institute",
+                image: "aspire-institute.jpg",
+                description: "For students who can already read Al-Quran and want to start to understand Al-Quran from basics.",
+                website:"https://aspireinstitute.com/"
+            },
+            {
+                name: "Darul Huffaz",
+                image: "darul-huffaz.jpg",
+                description: "We are a dedicated group of Quran teachers with the common objective of promoting Quranic reading and its proficiency and understanding among the local Muslims. We also offer courses that augment our core programmes and promote understanding and awareness of Islam.",
+                website: "http://darulhuffaz.sg/"
+            },
+            {
+                name: "Darul Quran",
+                image: "darul-quran.jpg",
+                description: "Menyediakan pendidikan Islam dengan kesungguhan, kesabaran dan keikhlasan dalam melahirkan Muslim yang berilmu dan bertaqwa berteraskan al-Quran dan al-Sunnah.",
+                website:"https://www.mks.org.sg/darul-quran/"
+            },
+            {
+                name: "Yameen",
+                image: "yameen.jpg",
+                description: "Using a systematic and structured approach, we aim to provide a safe learning environment for beginners or those who have lost touch in learning the Quran. ",
+                website:"http://www.yameen.sg/",
+            },
+            {
+                name: "Muhd Zuhaili",
+                image: "muhd-zuhaili.jpg",
+                description: "Welcome to MZ Services Singapore",
+                website:"https://mzuhailiservices.wordpress.com/about/"
+
+            },
+        ]
     }
   })
