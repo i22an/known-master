13b66f6cc6139a7e25892562a8ab14ed7d901bb1
ayman <aymanabdullahjamal@gmail.com>
2019-11-09 15:13:52 +0800

    added areas of centres

--- js/vue.js
+++ js/vue.js
@@ -7,7 +7,7 @@ var app = new Vue({
                 image: "i-read-quran-centre.jpg",
                 description: "We provide a secure and conducive environment for the holistic development of each child physically, socially, morally, emotionally and intellectually.",
                 website: "http://i-readquran.org/",
-                area:"",
+                area:"East",
                 subjects:[],
                 levels:[],
             },
@@ -16,7 +16,7 @@ var app = new Vue({
                 image: "andalus.jpg",
                 description: "Menyediakan pendidikan Islam dengan kesungguhan, kesabaran dan keikhlasan dalam melahirkan Muslim yang berilmu dan bertaqwa berteraskan al-Quran dan al-Sunnah.",
                 website:"http://www.andalus.sg/webandalus/index.php",
-                area:"",
+                area:['East','West','Central','North'],
                 subjects:[],
                 levels:[],
             },
@@ -25,7 +25,7 @@ var app = new Vue({
                 image: "aspire-institute.jpg",
                 description: "For students who can already read Al-Quran and want to start to understand Al-Quran from basics.",
                 website:"https://aspireinstitute.com/",
-                area:"",
+                area:['East','North','West'],
                 subjects:[],
                 levels:[],
             },
@@ -34,7 +34,7 @@ var app = new Vue({
                 image: "darul-huffaz.jpg",
                 description: "We are a dedicated group of Quran teachers with the common objective of promoting Quranic reading and its proficiency and understanding among the local Muslims. We also offer courses that augment our core programmes and promote understanding and awareness of Islam.",
                 website: "http://darulhuffaz.sg/",
-                area:"",
+                area:"East",
                 subjects:[],
                 levels:[],
             },
@@ -43,7 +43,7 @@ var app = new Vue({
                 image: "darul-quran.jpg",
                 description: "Menyediakan pendidikan Islam dengan kesungguhan, kesabaran dan keikhlasan dalam melahirkan Muslim yang berilmu dan bertaqwa berteraskan al-Quran dan al-Sunnah.",
                 website:"https://www.mks.org.sg/darul-quran/",
-                area:"",
+                area:"East",
                 subjects:[],
                 levels:[],
             },
@@ -52,7 +52,7 @@ var app = new Vue({
                 image: "yameen.jpg",
                 description: "Using a systematic and structured approach, we aim to provide a safe learning environment for beginners or those who have lost touch in learning the Quran. ",
                 website:"http://www.yameen.sg/",
-                area:"",
+                area:"Central",
                 subjects:[],
                 levels:[],
             },
@@ -61,10 +61,25 @@ var app = new Vue({
                 image: "muhd-zuhaili.jpg",
                 description: "Welcome to MZ Services Singapore",
                 website:"https://mzuhailiservices.wordpress.com/about/",
-                area:"",
+                area:"East",
                 subjects:[],
                 levels:[],
             },
-        ]
+        ],
+        areaFilter:  ""
+    },
+    methods: {
+        setAreaFilter: function(area) {
+           this.areaFilter= area;
+        }
+    },
+    computed: {
+        filteredCentres: function() {
+            if (!this.areaFilter) {
+                return this.centres;
+            } else {
+                return this.centres.filter(centre => centre.area === this.areaFilter)
+            }
+        }
     }
   })
